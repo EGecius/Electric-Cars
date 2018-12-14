26f885dcf60508efab9d5ca8e8a0be2b32d39c26
Egidijus Gecius <e.gecius@gmail.com>
2018-12-14 16:07:10 +0000

    add failing test to check if Circle CI works

---
+++ app/src/test/java/com/egecius/electriccars/MainActivityTest.kt
@@ -0,0 +1,22 @@
+package com.egecius.electriccars
+
+import org.junit.Before
+import org.junit.Test
+
+class MainActivityTest {
+
+    private var mSut: MainActivity? = null
+
+    @Before
+    fun setUp() {
+        mSut = MainActivity()
+    }
+
+    @Test
+    fun failed() {
+
+        assert(false)
+
+    }
+
+}
