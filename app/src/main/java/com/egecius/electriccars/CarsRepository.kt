331e549919abb502a6de4a595ca9191b3957e440
Egidijus Gecius <e.gecius@gmail.com>
2018-12-15 08:06:28 +0000

    Convert CarsRepository to return Rx Single

--- app/src/main/java/com/egecius/electriccars/CarsRepository.kt
+++ app/src/main/java/com/egecius/electriccars/CarsRepository.kt
@@ -1,16 +1,27 @@
 package com.egecius.electriccars
 
+import io.reactivex.Single
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.http.GET
 
 class CarsRepository {
 
-    fun setupRetrofit() : CarsRetrofitService {
+    private val retrofitService = setupRetrofit()
+
+    fun getCars(): Single<List<Car>> {
+        return retrofitService.cars()
+    }
+
+    private fun setupRetrofit(): CarsRetrofitService {
 
         // TODO: 15/12/2018 pass correct base url
 
         return Retrofit.Builder()
             .baseUrl("localhost")
+            .addConverterFactory(MoshiConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .build()
             .create(CarsRetrofitService::class.java)
     }
@@ -20,6 +31,6 @@ class CarsRepository {
 interface CarsRetrofitService {
 
     @GET("/electric_cars")
-    fun cars(): List<Car>
+    fun cars(): Single<List<Car>>
 
 }
