8b152178fc72d74d67e678780f5318da253ca4a2
Egidijus Gecius <e.gecius@gmail.com>
2018-12-15 12:33:04 +0000

    write/read at least one car to DB

--- app/src/main/java/com/egecius/electriccars/CarsRepository.kt
+++ app/src/main/java/com/egecius/electriccars/CarsRepository.kt
@@ -2,8 +2,11 @@ package com.egecius.electriccars
 
 import android.util.Log
 import com.egecius.electriccars.room.Car
+import com.egecius.electriccars.room.CarRoom
 import com.egecius.electriccars.room.CarsDatabase
 import io.reactivex.Single
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.schedulers.Schedulers
 
 class CarsRepository(retrofitAdapter: RetrofitAdapter, private val carsDatabase: CarsDatabase) {
 
@@ -17,18 +20,21 @@ class CarsRepository(retrofitAdapter: RetrofitAdapter, private val carsDatabase:
     }
 
     private fun storeCarsInDatabase(cars: List<Car>) {
-    	Log.v("Eg:CarsRepository:22", "storeCarsInDatabase")
+    	Log.d("Eg:CarsRepository:23", "storeCarsInDatabase cars $cars")
 
-//        for (car in cars) {
-//            carsDatabase.carDao().insertCar(car)
-//        }
-//
-//        val disposable = carsDatabase.carDao().car
-//            .subscribeOn(Schedulers.io())
-//            .observeOn(AndroidSchedulers.mainThread())
-//            .subscribe { it ->
-//                Log.i("Eg:CarsRepository:26", "storeCarsInDatabase car: $it")
-//            }
+        val carDao = carsDatabase.carDao()
+        for (car in cars) {
+            carDao.insertCar(CarRoom(car))
+        }
+
+        val disposable = carDao.car
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe { it ->
+                Log.i("Eg:CarsRepository:26", "storeCarsInDatabase car: $it")
+            }
+
+
     }
 
 }
