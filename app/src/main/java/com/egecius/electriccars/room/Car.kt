3d960ac14263ef8af80c22fb41363f6a27f84410
Egidijus Gecius <e.gecius@gmail.com>
2018-12-15 13:08:14 +0000

    remove redundant annotations in Car

--- app/src/main/java/com/egecius/electriccars/room/Car.kt
+++ app/src/main/java/com/egecius/electriccars/room/Car.kt
@@ -1,16 +1,11 @@
 package com.egecius.electriccars.room
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "cars")
 data class Car(
-
     @PrimaryKey
-    @ColumnInfo(name = "name")
     val name: String,
-
-    @ColumnInfo(name = "img")
     val img: String
 )
