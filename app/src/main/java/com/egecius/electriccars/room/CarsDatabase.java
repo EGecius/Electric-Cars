6625dd708fed6679d9cc6007faf86ad62ae108b1
Egidijus Gecius <e.gecius@gmail.com>
2018-12-15 11:52:52 +0000

    remove Room's warning on schemas

--- app/src/main/java/com/egecius/electriccars/room/CarsDatabase.java
+++ app/src/main/java/com/egecius/electriccars/room/CarsDatabase.java
@@ -5,7 +5,7 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {Car.class}, version = 1)
+@Database(entities = {Car.class}, version = 1, exportSchema = false)
 public abstract class CarsDatabase extends RoomDatabase {
 
     public abstract CarDao carDao();
