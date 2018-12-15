d45fb62279df0c47dee04759f83e4d6b4d87a90e
Egidijus Gecius <e.gecius@gmail.com>
2018-12-15 15:06:23 +0000

    show cars loaded from DB

--- app/src/main/java/com/egecius/electriccars/repository/CarsRepository.kt
+++ app/src/main/java/com/egecius/electriccars/repository/CarsRepository.kt
@@ -4,30 +4,30 @@ import android.util.Log
 import com.egecius.electriccars.retrofit.RetrofitAdapter
 import com.egecius.electriccars.room.Car
 import com.egecius.electriccars.room.CarsDatabase
+import io.reactivex.Completable
 import io.reactivex.Single
 
-class CarsRepository(retrofitAdapter: RetrofitAdapter, private val carsDatabase: CarsDatabase) {
+class CarsRepository(retrofitAdapter: RetrofitAdapter, carsDatabase: CarsDatabase) {
 
     private val retrofitService = retrofitAdapter.setupRetrofit()
+    private val carDao = carsDatabase.carDao()!!
 
     fun getCars(): Single<List<Car>> {
         return retrofitService.cars()
-            .doOnSuccess {
-                storeCarsInDatabase(it)
+            .flatMapCompletable {
+                Completable.fromAction {
+                    storeCarsInDatabase(it)
+                }
+            }.toSingle {
+                carDao.loadAllCars()
             }
     }
 
     private fun storeCarsInDatabase(cars: List<Car>) {
-
-        val carDao = carsDatabase.carDao()
         for (car in cars) {
             Log.v("Eg:CarsRepository:27", "storeCarsInDatabase inserting: ${car.name}")
             carDao.insertCar(car)
         }
-
-        val allCars = carDao.loadAllCars()
-        Log.i("Eg:CarsRepository:29", "storeCarsInDatabase allCars.size " + allCars.size)
-        Log.i("Eg:CarsRepository:38", "storeCarsInDatabase allCars: $allCars")
     }
 
 }
