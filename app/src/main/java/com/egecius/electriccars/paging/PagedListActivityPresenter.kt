5a4af51212d29ab8ce90f0b001f19843df48c158
Egidijus Gecius <e.gecius@gmail.com>
2018-12-19 09:54:47 +0000

    move dependencies of PagedListActivityPresenter to Module

--- app/src/main/java/com/egecius/electriccars/paging/PagedListActivityPresenter.kt
+++ app/src/main/java/com/egecius/electriccars/paging/PagedListActivityPresenter.kt
@@ -4,33 +4,24 @@ import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
-import androidx.paging.LivePagedListBuilder
 import androidx.paging.PagedList
-import com.egecius.electriccars.retrofit.RetrofitAdapter
 import com.egecius.electriccars.room.Car
 
 class PagedListActivityPresenter : ViewModel() {
+
+    private lateinit var carsLiveData: LiveData<PagedList<Car>>
+
+    fun init(carsLiveData: LiveData<PagedList<Car>>) {
+        this.carsLiveData = carsLiveData
+    }
 
     fun startPresenting(
         lifecycleOwner: LifecycleOwner,
         view: PagedListActivityView
     ) {
-        getCarsLiveData().observe(lifecycleOwner, Observer { cars ->
+        carsLiveData.observe(lifecycleOwner, Observer { cars ->
             view.showCars(cars)
         })
-    }
-
-    private fun getCarsLiveData(): LiveData<PagedList<Car>> {
-
-        val carsRetrofitService = RetrofitAdapter().setupRetrofit()
-        val myDataSourceFactory = MyDataSourceFactory(carsRetrofitService)
-
-        val config = PagedList.Config.Builder()
-            .setEnablePlaceholders(false)
-            .setInitialLoadSizeHint(5)
-            .setPageSize(5).build()
-
-        return LivePagedListBuilder(myDataSourceFactory, config).build()
     }
 
 }
--- app/src/main/java/com/egecius/electriccars/paging/di/PagedListActivityDi.kt
+++ app/src/main/java/com/egecius/electriccars/paging/di/PagedListActivityDi.kt
@@ -1,8 +1,14 @@
 package com.egecius.electriccars.paging.di
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProviders
+import androidx.paging.LivePagedListBuilder
+import androidx.paging.PagedList
+import com.egecius.electriccars.paging.MyDataSourceFactory
 import com.egecius.electriccars.paging.PagedListActivity
 import com.egecius.electriccars.paging.PagedListActivityPresenter
+import com.egecius.electriccars.retrofit.RetrofitAdapter
+import com.egecius.electriccars.room.Car
 import dagger.Component
 import dagger.Module
 import dagger.Provides
@@ -17,7 +23,24 @@ interface PagedListActivityComponent {
 class PagedListActivityModule(private val activity: PagedListActivity) {
 
     @Provides
-    fun providesPresenter(): PagedListActivityPresenter {
-        return ViewModelProviders.of(activity).get(PagedListActivityPresenter::class.java)
+    fun providesPresenter(carsLiveData: LiveData<PagedList<Car>>): PagedListActivityPresenter {
+        val presenter = ViewModelProviders.of(activity).get(PagedListActivityPresenter::class.java)
+        presenter.init(carsLiveData)
+        return presenter
+    }
+
+    @Provides
+    fun providesCarsLiveData(): LiveData<PagedList<Car>> {
+
+        val carsRetrofitService = RetrofitAdapter().setupRetrofit()
+        val myDataSourceFactory = MyDataSourceFactory(carsRetrofitService)
+
+        val config = PagedList.Config.Builder()
+            .setEnablePlaceholders(false)
+            .setInitialLoadSizeHint(5)
+            .setPageSize(5)
+            .build()
+
+        return LivePagedListBuilder(myDataSourceFactory, config).build()
     }
 }
