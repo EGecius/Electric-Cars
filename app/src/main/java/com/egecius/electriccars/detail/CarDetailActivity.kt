e80eb8a2ac24e8f4bb091c0f6d6a839e2c3c63f2
Egidijus Gecius <e.gecius@gmail.com>
2018-12-20 07:38:04 +0000

    add CarDetailActivity

---
+++ app/src/main/java/com/egecius/electriccars/detail/CarDetailActivity.kt
@@ -0,0 +1,14 @@
+package com.egecius.electriccars.detail
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.egecius.electriccars.R
+
+class CarDetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_car_detail)
+    }
+
+}
