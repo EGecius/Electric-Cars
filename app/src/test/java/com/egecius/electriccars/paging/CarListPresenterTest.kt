b8e8b150120f53ea15f7d977ffd9261d8a16ab75
Egidijus Gecius <e.gecius@gmail.com>
2019-03-08 08:45:32 +0000

    add tests for CarListPresenter

---
+++ app/src/test/java/com/egecius/electriccars/paging/CarListPresenterTest.kt
@@ -0,0 +1,54 @@
+package com.egecius.electriccars.paging
+
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.paging.PagedList
+import com.egecius.electriccars.room.Car
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.verify
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.ArgumentCaptor
+import org.mockito.Captor
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class CarListPresenterTest {
+
+    private lateinit var mSut: CarListPresenter
+
+    @Mock
+    lateinit var carsLiveData : LiveData<PagedList<Car>>
+    @Mock
+    lateinit var view: CarListPresenter.View
+    @Mock
+    lateinit var lifecycleOwner: LifecycleOwner
+    @Mock
+    lateinit var pagedListCars: PagedList<Car>
+    @Captor
+    lateinit var argumentCaptor: ArgumentCaptor<Observer<PagedList<Car>>>
+
+    @Before
+    fun setUp() {
+        mSut = CarListPresenter()
+        mSut.init(carsLiveData)
+    }
+
+    @Test
+    fun `shows list of cars`() {
+        mSut.startPresenting(view, lifecycleOwner)
+
+        whenLiveDataReturnsResult()
+
+        verify(view).showCars(pagedListCars)
+    }
+
+    private fun whenLiveDataReturnsResult() {
+        verify(carsLiveData).observe(eq(lifecycleOwner), argumentCaptor.capture())
+        argumentCaptor.value.onChanged(pagedListCars)
+    }
+
+}
