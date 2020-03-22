1ad72746144682f1d7f10d52fc49c5a8cd01460b
Egidijus Gecius <e.gecius@gmail.com>
2020-03-22 16:50:02 +0000

    remove redundant code in MainActivityViewModel.kt

--- app/src/main/java/com/egecius/electriccars/mainactivity/MainActivityViewModel.kt
+++ app/src/main/java/com/egecius/electriccars/mainactivity/MainActivityViewModel.kt
@@ -1,11 +1,8 @@
 package com.egecius.electriccars.mainactivity
 
-import android.util.Log
 import androidx.lifecycle.*
 import com.egecius.electriccars.repository.CarsRepository
 import com.egecius.electriccars.room.Car
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
 
 class MainActivityViewModel : ViewModel() {
 
@@ -22,19 +19,6 @@ class MainActivityViewModel : ViewModel() {
     ) {
         this.view = view
         showCars(lifecycleOwner)
-        startInfiniteLoop(lifecycleOwner)
-    }
-
-    private fun startInfiniteLoop(lifecycleOwner: LifecycleOwner) {
-        lifecycleOwner.lifecycleScope.launch {
-            val start = System.currentTimeMillis()
-            while (true) {
-                delay(1000)
-                val diff = System.currentTimeMillis() - start
-                // see how this gets cancelled when the app goes into the background
-                Log.v("Eg:MainActivityViewModel:33", "startInfiniteLoop() diff: $diff")
-            }
-        }
     }
 
     private fun showCars(lifecycleOwner: LifecycleOwner) {
